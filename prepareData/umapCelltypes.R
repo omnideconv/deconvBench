5e049d66695c690b6be1976067e36aba04b68034
Katharina Reinisch <k.reinisch@norm.exbio.wzw.tum.de>
2022-01-25 16:54:29 +0100

    update umap

--- prepareData/umapCelltypes.R
+++ prepareData/umapCelltypes.R
@@ -16,16 +16,21 @@ sc_matrix <- readRDS(file.path(sc_path, sc_ds, "matrix.rds"))
 sc_celltype_annotations <- readRDS(file.path(sc_path, sc_ds, "celltype_annotations.rds"))
 remapping_sheet <- args$remapping_sheet
 
+u <- umap::umap(t(as.matrix(sc_matrix)))
+df <- data.frame(u$layout)
+df$celltype <- sc_celltype_annotations
+p <- ggplot(data = df, aes(X1, X2, color=celltype)) + geom_point() + labs(title=paste("UMAP", sc_ds))
+p
+ggsave(paste("umap_singlecell_", sc_ds, "_oldNames.jpg", sep=""), p, width = 10, height = 6)
+
 source("/nfs/proj/omnideconv_benchmarking/benchmark/pipeline/bin/remapCelltypesNF.R")
 sc_celltype_annotations <- remapCelltypesWorkflow(remappingPath = remapping_sheet, 
                                                   celltype_annotations = sc_celltype_annotations, 
                                                   method_ds = sc_ds)
-u <- umap::umap(t(as.matrix(sc_matrix)))
-df <- data.frame(u$layout)
 df$celltype <- sc_celltype_annotations
 p <- ggplot(data = df, aes(X1, X2, color=celltype)) + geom_point() + labs(title=paste("UMAP", sc_ds))
 p
 ggsave(paste("umap_singlecell_", sc_ds, ".jpg", sep=""), p)
 
 ##maynard: looks almost good
-##lambrechts: many NAs have to work on mapping again!
+##lambrechts: t cell cd4 and cd8 aufgeteilt?
